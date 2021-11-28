c68cc6ec6a975ac116ddd4348e8c6c070bf2c35c
ronieo <romelzen50@gmail.com>
2021-11-28 22:07:26 +0900

    make xylophon with Function

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/angelayu/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/angelayu/AndroidStudioProjects/xylophone-flutter"
+export "FLUTTER_ROOT=/Users/iloun/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/iloun/StudioProjects/-flutter3-xylophone"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/angelayu/AndroidStudioProjects/xylophone-flutter/lib/main.dart"
+export "FLUTTER_TARGET=/Users/iloun/StudioProjects/-flutter3-xylophone/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
@@ -11,4 +11,4 @@ export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVF
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/angelayu/AndroidStudioProjects/xylophone-flutter/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/iloun/StudioProjects/-flutter3-xylophone/.dart_tool/package_config.json"
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(int noteNum) {
+    final player = AudioCache();
+    player.play('note$noteNum.wav');
+  }
+
+  Expanded buildXylophone({Color color, int noteNum}) {
+   return Expanded(
+      child: Container(
+        color: color,
+        child: TextButton(
+          onPressed: (){
+            playSound(noteNum);
+          },
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                buildXylophone(color: Colors.red, noteNum: 1),
+                buildXylophone(color: Colors.orange, noteNum: 2),
+                buildXylophone(color: Colors.yellow, noteNum: 3),
+                buildXylophone(color: Colors.green, noteNum: 4),
+                buildXylophone(color: Colors.blue, noteNum: 5),
+                buildXylophone(color: Colors.indigo, noteNum: 6),
+                buildXylophone(color: Colors.purple, noteNum: 7),
+
+              ],
+            ),
         ),
       ),
     );
